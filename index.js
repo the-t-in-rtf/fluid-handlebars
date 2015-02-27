d290cce0ec860d2d68353597ff1918ff3480b414
Tony <tony@raisingthefloor.org>
2015-02-27 11:49:31 +0100

    Moved index.js to allow "requiring" of entire package.

---
+++ index.js
@@ -0,0 +1,13 @@
+// A convenience file to include all server-side components at once
+
+// Common base components for both server and client side
+require("./src/js/common/helper");
+require("./src/js/common/jsonify");
+require("./src/js/common/md-common");
+
+// Server-side components
+require("./src/js/server/dispatcher");
+require("./src/js/server/inline");
+require("./src/js/server/md-server");
+require("./src/js/server/handlebars");
+
