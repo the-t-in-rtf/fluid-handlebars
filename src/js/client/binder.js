e3f66881a266297007fe3d2e8b8ecc83d61e0cf8
Tony Atkins <duhrer@gmail.com>
2015-06-09 14:16:48 +0200

    GPII-1175:  Updated binder based on feedback from pull request.

--- src/js/client/binder.js
+++ src/js/client/binder.js
@@ -3,9 +3,7 @@
 // To use this, you should have a "bindings" element in your fluid defaults, as in:
 //      bindings: [{
 //          selector:    "input",
-//          path:        "input",
-//          elementType: "text",
-//          cookify:     true
+//          path:        "input"
 //      }]
 //
 //
@@ -15,11 +13,6 @@
 //
 // * path:        A valid path for the model variable whose value will be watched.
 //                Must be able to be resolved using fluid.get(path)
-//
-// * elementType: The type of html element (typically form elements) that allows the user to control the value.
-//                Only "radio" is meaningful at the moment, as those are handled differently.
-//                For everything else, the value is set using element.val(value).
-//
 //
 // To make use of this, you will need to start by sourcing this file after all fluid sources.
 //
@@ -48,6 +41,8 @@
 // Once you have done this, model changes should be passed to form controls and vice versa.
 //
 // Note that if you reload markup or otherwise generate new markup, you will need to reapply the set of bindings.
+// The `templateAware` grade included with this package takes care of that for you by calling `fluid.initDomBinder`
+// and then running `applyBinding` once the `onDomBind` event is fired.  See that component for more details.
 //
 // This code was originally written by Antranig Basman <amb26@ponder.org.uk> and was evolved by Tony Atkins
 // <tony@raisingthefloor.org> to document its use and make it usable with a wider range of form element types.
@@ -57,13 +52,6 @@
     "use strict";
     fluid.registerNamespace("gpii.templates.binder");
 
-    // A convenience function to handle setting of radio buttons.
-    gpii.templates.binder.setRadioValue = function (element, change) {
-        element.each(function (index, option) {
-            option.checked = ($(option).val() === change);
-        });
-    };
-
     // The main function to create bindings between markup and model elements.  See above for usage details.
     gpii.templates.binder.applyBinding = function (that) {
         var bindings = that.options.bindings;
@@ -72,26 +60,13 @@
 
             var value = fluid.get(that.model, binding.path);
 
-            // initial sync, model overwrites values
-            if (binding.elementType === "radio") {
-                gpii.templates.binder.setRadioValue(element, value);
-            }
-            else {
-                element.val(value);
-            }
+            fluid.value(element, value);
 
             // Update the model when the form changes
             element.change(function () {
                 console.log("Changing model based on element update.");
 
-                var value = element.val();
-                if (binding.elementType === "radio") {
-                    element.each(function (index, option) {
-                        if (option.checked) {
-                            value = $(option).val();
-                        }
-                    });
-                }
+                var value = fluid.value(element);
                 that.applier.change(binding.path, value);
             });
 
@@ -99,12 +74,7 @@
             that.applier.modelChanged.addListener(binding.path, function (change) {
                 console.log("Changing value based on model update.");
 
-                if (binding.elementType === "radio") {
-                    gpii.templates.binder.setRadioValue(element, change);
-                }
-                else {
-                    element.val(change);
-                }
+                fluid.value(element, change);
             });
         });
     };
