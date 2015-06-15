25b4cdbba8fcc8032f6a054ca0bca7f092e048b9
Tony Atkins <duhrer@gmail.com>
2015-06-15 13:41:23 +0200

    GPII-1175:  Finished refactoring `binder` to support better configuration options and added tests independent of template handling.

--- src/js/client/binder.js
+++ src/js/client/binder.js
@@ -1,22 +1,29 @@
 // Add persistent bindings between a selector and a model value.  Changes are propagated between the two.
 //
-// To use this, you should have a "bindings" element in your fluid defaults, as in:
-//      bindings: [{
-//          selector:    "input",
-//          path:        "input"
-//      }]
+// To use this, you should add have an `options.bindings` element to your component.  It should look something like:
 //
+// bindings: {
+//   "key": {
+//     selector: "selector1",
+//     path:     "path1",
+//   },
+//   "selector2": "path2"
+// }
 //
-// The options are as follows:
+// There are two ways of specifying bindings.  The "long form" has named keys (as in the first example above) and
+// supports the following options:
+//
 // * selector:    A valid selector for your component.
 //                Must be able to be resolved using that.locate(selector)
 //
 // * path:        A valid path for the model variable whose value will be watched.
 //                Must be able to be resolved using fluid.get(path)
 //
-// To make use of this, you will need to start by sourcing this file after all fluid sources.
+// The "short form" uses the selector as the key, and the path as a string value (as in the second example above).
 //
-// In your component, you will need a valid selector, as in:
+// To make use of either form, you will need to start by sourcing this file after all fluid sources.
+//
+// Then, in your component, you will need a valid selector, as in:
 //
 //  selectors: {
 //      "input":    ".ptd-search-input"
@@ -33,19 +40,19 @@
 //
 //  listeners: {
 //      onCreate: {
-//        "funcName": "ctr.components.binder.applyBinding",
+//        "funcName": "gpii.templates.binder.applyBinding",
 //        "args":     "{that}"
 //      }
 //  }
 //
-// Once you have done this, model changes should be passed to form controls and vice versa.
+// Once you have done all this, model changes should be passed to form controls and vice versa.
 //
 // Note that if you reload markup or otherwise generate new markup, you will need to reapply the set of bindings.
 // The `templateAware` grade included with this package takes care of that for you by calling `fluid.initDomBinder`
-// and then running `applyBinding` once the `onDomBind` event is fired.  See that component for more details.
+// and then running `applyBinding` once the `onDomBind` event is fired.  See that component for a working example.
 //
-// This code was originally written by Antranig Basman <amb26@ponder.org.uk> and was evolved by Tony Atkins
-// <tony@raisingthefloor.org> to document its use and make it usable with a wider range of form element types.
+// This code was originally written by Dr. Antranig Basman <amb26@ponder.org.uk> and with his advice was updated and
+// extended by Tony Atkins <tony@raisingthefloor.org>.
 //
 /* global fluid, jQuery */
 (function ($) {
@@ -55,27 +62,50 @@
     // The main function to create bindings between markup and model elements.  See above for usage details.
     gpii.templates.binder.applyBinding = function (that) {
         var bindings = that.options.bindings;
-        fluid.each(bindings, function (binding) {
-            var element = that.locate(binding.selector);
-
-            var value = fluid.get(that.model, binding.path);
-
-            fluid.value(element, value);
+        fluid.each(bindings, function (value, key) {
+            var path     = typeof value === "string" ? value : value.path;
+            var selector = typeof value === "string" ? key : value.selector;
+            var type     = value.type ? value.type : "text";
+            var element = that.locate(selector);
 
             // Update the model when the form changes
             element.change(function () {
                 fluid.log("Changing model based on element update.");
 
-                var value = fluid.value(element);
-                that.applier.change(binding.path, value);
+                var elementValue = fluid.value(element);
+                that.applier.change(path, elementValue);
             });
 
             // Update the form elements when the model changes
-            that.applier.modelChanged.addListener(binding.path, function (change) {
+            that.applier.modelChanged.addListener(path, function (change) {
                 fluid.log("Changing value based on model update.");
 
-                fluid.value(element, change);
+                // We must use a separate method to update radio buttons
+                if (type === "radio") {
+                    element.val(change[path]);
+                    element.each(function (index, button) {
+                        if (button.value === change[path]) {
+                            button.checked = true;
+                        }
+                    });
+                }
+                else {
+                    fluid.value(element, change[path]);
+                }
             });
+
+            // If we have model data initially, update the form.  Model values win out over markup.
+            var initialModelValue = fluid.get(that.model, path);
+            if (initialModelValue) {
+                fluid.value(element, initialModelValue);
+            }
+            // If we have no model data, but there are defaults in the markup, using them to update the model.
+            else {
+                var initialFormValue = fluid.value(element);
+                if (initialFormValue) {
+                    that.applier.change(path, initialFormValue);
+                }
+            }
         });
     };
 })(jQuery);
---
+++ tests/js/zombie-binder-tests.js
@@ -0,0 +1,87 @@
+"use strict";
+// Test "binder" components using Zombie and filesystem content.
+//
+// Zombie provides two means of interrogating globals within the browser, namely:
+//
+// 1. `browser.assert.global(name, expected, message)`, which tests the global with `name` to confirm that it matches
+//     `expected`, and throws `message` if it does not.
+//
+// 2. `browser.window[fieldName]` provides direct access to the global variable `fieldName`.
+//
+// We will use the latter in these tests and using `jqUnit` assertions.  The page itself will use jQuery to manipulate
+// all controls, Zombie is only inspecting the final results.
+//
+// TODO:  Revisit opening a standalone file with Zombie once we have a better pattern for using it with server content.
+var fluid = fluid || require("infusion");
+var gpii  = fluid.registerNamespace("gpii");
+
+fluid.setLogging(true);
+
+var Browser = require("zombie");
+var path    = require("path");
+var jqUnit = require("jqUnit");
+
+fluid.registerNamespace("gpii.hb.tests.binder");
+gpii.hb.tests.binder.runTests = function (that) {
+    jqUnit.module("Testing binder separately from template handling...");
+    fluid.each(that.options.componentNames, function (componentName) {
+        jqUnit.asyncTest("Testing component '" + componentName + "'...", function () {
+            var browser = Browser.create();
+            browser.on("error", function (error) {
+                jqUnit.start();
+                jqUnit.fail("There should be no errors:" + error);
+            });
+            browser.visit(that.options.url, function () {
+                jqUnit.start();
+                var component = browser.window[componentName];
+                jqUnit.assertDeepEq("The initial model of component '" + componentName + "' should match the expected results.", that.options.expectedOnInit, component.model);
+
+                component.applier.change("updateFromModel", "updated using applier");
+                var updatedFormElement = component.locate("updateFromModel");
+                jqUnit.assertEquals("A change to the model should result in an update to the form element", "updated using applier", updatedFormElement.val());
+
+                var formElementToUpdate = component.locate("updateFromMarkup");
+                // Zombie's `fill(selector, value)` method only works with particular types of elements, so we have to use a range of approaches for other types of form elements.
+                var tag = formElementToUpdate.prop("tagName").toLowerCase();
+                var type = formElementToUpdate.prop("type");
+                if (tag === "textarea" || (tag === "input" && type === "text")) {
+                    browser.fill(formElementToUpdate.selector, "updated using form controls");
+                }
+                // Zombie unhelpfully does not allow you to click the right radio button from the group of possible values.  Instead, you have to `choose` an appropriate selector.
+                else if (tag === "input" && type === "radio") {
+                    formElementToUpdate.each(function (index, button) {
+                        if (button.value === "updated using form controls") {
+                            // Click the button with the right id.
+                            browser.choose("#" + button.id);
+                        }
+                    });
+                }
+                // Thankfully, Zombie at least allows choosing a select option matching a desired value.
+                else if (tag === "select") {
+                    browser.select(formElementToUpdate.selector, "updated using form controls");
+                }
+                jqUnit.assertEquals("A change to the form controls should result in an update to the model", "updated using form controls", component.model.updateFromMarkup);
+            });
+        });
+    });
+};
+
+var testFile = "file://" + path.resolve(__dirname, "../html/tests-binder.html");
+fluid.defaults("gpii.hb.tests.binder", {
+    gradeNames: ["fluid.eventedComponent", "autoInit"],
+    url:        testFile,
+    componentNames: ["long", "short", "array", "textarea", "select", "radio"],
+    expectedOnInit: {
+        initFromModel:    "initialized from model",
+        initFromMarkup:   "initialized from markup"
+    },
+    listeners: {
+        "onCreate.runTests": {
+            funcName: "gpii.hb.tests.binder.runTests",
+            args:     ["{that}"]
+        }
+    }
+});
+
+
+gpii.hb.tests.binder();
