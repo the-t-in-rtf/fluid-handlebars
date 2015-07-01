40a08bb7f22721f3b3fc7aa03958297216be29e4
Tony Atkins <duhrer@gmail.com>
2015-07-01 13:23:07 +0200

    GPII-1225:  Removed handling for errors that are served with a 200 status code.  Added batching of model changes.  Improved variable names.

--- src/js/client/ajaxCapable.js
+++ src/js/client/ajaxCapable.js
@@ -34,24 +34,28 @@ If you need to do both (or each multiple times), you should create a parent comp
     var gpii = fluid.registerNamespace("gpii");
     fluid.registerNamespace("gpii.templates.ajaxCapable");
 
+    // Apply all changes in a single transaction.  Also ensures that values flagged with `null` are deleted from the model.
+    gpii.templates.ajaxCapable.batchChanges = function (that, changeSet) {
+        var myTransaction = that.applier.initiate();
 
-    // TODO: Replace this with JSON Schema validation: https://issues.gpii.net/browse/GPII-1176
-    gpii.templates.ajaxCapable.checkRequirements = function (that) {
-        var errors = [];
+        fluid.each(changeSet, function (value, key) {
+            var change = { path: key };
+            if (value === undefined || value === null) {
+                change.type = "DELETE";
+            }
+            else {
+                change.value = value;
+            }
+            myTransaction.fireChangeRequest(change);
+        });
 
-        if (!that.options.rules || !that.options.rules.error || !that.options.rules.success || !that.options.rules.request) {
-            errors.push("You have not configured the required rules regarding how to handle AJAX responses.");
-        }
-
-        if (errors.length > 0) {
-            fluid.fail(errors);
-        }
+        myTransaction.commit();
     };
 
     gpii.templates.ajaxCapable.makeRequest = function (that) {
         var options = fluid.copy(that.options.ajaxOptions);
 
-        var transformedModel = fluid.model.transformWithRules(that.model, that.options.rules.request);
+        var transformedModel = fluid.model.transformWithRules(that.model, that.options.rules.modelToRequestPayload);
 
         // We have added a `json` option not supported by `jQuery.ajax()` itself, which makes it easier to pass JSON data.
         if (options.json) {
@@ -63,32 +67,17 @@ If you need to do both (or each multiple times), you should create a parent comp
     };
 
     gpii.templates.ajaxCapable.handleSuccess = function (that, data) {
-        // We assume that we are working with a success unless we have explicit data that suggests otherwise.
-        var okData = fluid.model.transformWithRules(data, that.options.rules.ok);
-        if (okData.ok === undefined || okData.ok === null || okData.ok) {
-            var transformedData = fluid.model.transformWithRules(data, that.options.rules.success);
+        var transformedData = fluid.model.transformWithRules(data, that.options.rules.successResponseToModel);
 
-            // The transformed result is used to update the component's `model`.
-            Object.keys(transformedData).forEach(function (key) {
-                that.applier.change(key, transformedData[key]);
-            });
-        }
-        // If the response is not OK, pass it along to be handled as an error instead.
-        // NOTE:  The error handling has its own rules for parsing the original response, so we must pass the raw data.
-        else {
-            gpii.templates.ajaxCapable.handleError(that, data);
-        }
+        gpii.templates.ajaxCapable.batchChanges(that, transformedData);
 
         that.events.requestReceived.fire(that);
     };
 
     gpii.templates.ajaxCapable.handleError = function (that, data) {
-        var errorData = fluid.model.transformWithRules(data, that.options.rules.error);
+        var errorData = fluid.model.transformWithRules(data, that.options.rules.errorResponseToModel);
 
-        // The transformed result is used to update the component's `model`.
-        Object.keys(errorData).forEach(function (key) {
-            that.applier.change(key, errorData[key]);
-        });
+        gpii.templates.ajaxCapable.batchChanges(that, errorData);
 
         that.events.requestReceived.fire(that);
     };
@@ -104,23 +93,18 @@ If you need to do both (or each multiple times), you should create a parent comp
         },
         // Rules to control what (if any) feedback from successful response is displayed.
         rules: {
-            // Rules to evaluate whether a response is successful
-            ok: {
-                "": "responseJSON" // By default, use the entire jQuery `jqXHR` object's JSON payload.
-            },
-
             // Rules to control how a successful response is applied to the model.
-            success: {
+            successResponseToModel: {
                 "": "responseJSON" // By default, use the entire jQuery `jqXHR` object's JSON payload.
             },
 
             // Rules to control how an error is applied to the model
-            error: {
+            errorResponseToModel: {
                 "": "responseJSON" // By default, use the entire jQuery `jqXHR` object's JSON payload.
             },
 
             // Rules to control how our model is parsed before making a request
-            request: {
+            modelToRequestPayload: {
                 "": ""    // By default, pass the model with no alterations.
             }
         },
