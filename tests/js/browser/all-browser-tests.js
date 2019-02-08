451bed23e14014b204631a34606edae3892a2c57
Tony Atkins <tony@raisingthefloor.org>
2019-02-08 12:40:16 +0100

    GPII-3553: Added note about work required to collect complete code coverage data.

--- tests/js/browser/all-browser-tests.js
+++ tests/js/browser/all-browser-tests.js
@@ -1,6 +1,14 @@
 /* eslint-env node */
 "use strict";
 
+/*
+
+    TODO: Convert these tests to use gpii-testem once this feature is added:
+
+    https://issues.gpii.net/browse/GPII-3700
+
+ */
+
 require("./browser-initBlock-tests.js");
 require("./browser-inline-tests.js");
 require("./browser-renderer-serverAware-tests.js");
