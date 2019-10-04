c14d795ca61c711b54fa8d63e51736fba8ca3907
Tony Atkins <tony@raisingthefloor.org>
2019-10-04 12:13:39 +0200

    GPII-4100: Hardened templateAware renderer tests to avoid race condition and intermittent faillures.

--- tests/browser-fixtures/js/tests-renderer-serverResourceAware.js
+++ tests/browser-fixtures/js/tests-renderer-serverResourceAware.js
@@ -14,46 +14,61 @@
         that.events.onMarkupRendered.fire();
     };
 
-    fluid.registerNamespace("gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder");
+    // common sequences to ensure the component is actually ready to render for each run of tests.
+    fluid.defaults("gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.startSequence", {
+        gradeNames: "fluid.test.sequenceElement",
+        sequence: [
+            { // This sequence point is required because of a QUnit bug - it defers the start of sequence by 13ms "to avoid any current callbacks" in its words
+                func: "{testEnvironment}.events.constructFixtures.fire"
+            },
+            {
+                listener: "fluid.identity",
+                event: "{testEnvironment}.events.onFixturesConstructed"
+            }
+        ]
+    });
+
+    fluid.defaults("gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.stopSequence", {
+        gradeNames: "fluid.test.sequenceElement",
+        sequence: [{
+            func: "{testEnvironment}.destroyViewComponent"
+        }]
+    });
+
+    fluid.defaults("gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade", {
+        gradeNames: ["fluid.test.sequence"],
+        sequenceElements: {
+            startFixtures: {
+                gradeNames: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.startSequence",
+                priority: "before:sequence"
+            },
+            stopFixtures: {
+                gradeNames: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.stopSequence",
+                priority: "after:sequence"
+            }
+        }
+    });
 
     gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.getPreviousSibling = function () {
         return $(".viewport-before").prev();
     };
 
     fluid.defaults("gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder", {
-        gradeNames: ["fluid.test.testCaseHolder", "gpii.tests.handlebars.templateAware.serverResourceAware"],
-        model: {
-            myvar:    "modelvariable",
-            markdown: "*this works*",
-            json:     { foo: "bar", baz: "quux", qux: "quux" }
-        },
-        templateName:            "index",
+        gradeNames: ["fluid.test.testCaseHolder"],
         replaceWithTemplateName: "replace",
-        selectors: {
-            "viewport-after":       ".viewport-after",
-            "viewport-append":      ".viewport-append",
-            "viewport-before":      ".viewport-before",
-            "viewport-html":        ".viewport-html",
-            "viewport-prepend":     ".viewport-prepend",
-            "viewport-replaceWith": ".viewport-replaceWith"
-        },
-        invokers: {
-            "renderInitialMarkup": {
-                funcName: "fluid.identity"
-            }
-        },
         modules: [{
             name: "Tests for 'server resource aware' renderer.",
             tests: [
                 {
                     name: "Confirm that the client-side renderer can add content after an existing element.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "after", "viewport-after"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "after", "viewport-after"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckSelectors",
                             args: [".viewport-after + *", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -65,13 +80,14 @@
                 },
                 {
                     name: "Confirm that the client-side renderer can append content to an existing element.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "append", "viewport-append"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "append", "viewport-append"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckSelectors",
                             args: [".viewport-append", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -83,13 +99,14 @@
                 },
                 {
                     name: "Confirm that the client-side renderer can add content before an existing element.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "before", "viewport-before"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "before", "viewport-before"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckElements",
                             args: ["@expand:gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.getPreviousSibling()", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -101,13 +118,14 @@
                 },
                 {
                     name: "Confirm that the client-side renderer can replace existing html content in an element.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "html", "viewport-html"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "html", "viewport-html"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckSelectors",
                             args: [".viewport-html", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -119,13 +137,14 @@
                 },
                 {
                     name: "Confirm that the client-side renderer can prepend content to an existing element.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "prepend", "viewport-prepend"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "prepend", "viewport-prepend"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckSelectors",
                             args: [".viewport-prepend", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -137,13 +156,14 @@
                 },
                 {
                     name: "Confirm that the client-side renderer can replace an existing element altogether.",
+                    sequenceGrade: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder.sequenceGrade",
                     sequence: [
                         {
                             func: "gpii.tests.handlebars.renderer.serverResourceAware.renderBlock",
-                            args: ["{that}", "replaceWith", "viewport-replaceWith", "{that}.options.replaceWithTemplateName"] // that, rendererFn, selector, [templateName]
+                            args: ["{testEnvironment}.viewComponent", "replaceWith", "viewport-replaceWith", "{that}.options.replaceWithTemplateName"] // that, rendererFn, selector, [templateName]
                         },
                         {
-                            event: "{that}.events.onMarkupRendered",
+                            event: "{testEnvironment}.events.onMarkupRendered",
                             listener: "gpii.test.handlebars.browser.sanityCheckSelectors",
                             args: [".viewport-replaceWith.replaced", gpii.test.handlebars.browser.matchDefs.standard] // selector, matchDefs
                         },
@@ -157,12 +177,77 @@
         }]
     });
 
+    fluid.registerNamespace("gpii.tests.handlebars.renderer.serverResourceAware.testEnvironment");
+    gpii.tests.handlebars.renderer.serverResourceAware.testEnvironment.destroyViewComponent = function (testEnvironment) {
+        var viewComponent = fluid.get(testEnvironment, "viewComponent");
+        if (viewComponent) {
+            /*
+                If I don't manually destroy the component and swallow the error, tests fail with errors like:
+
+                `rec.event.removeListener is not a function`
+
+             */
+            // TODO: Discuss with Antranig.
+            try {
+                var destroyReturn = viewComponent.destroy();
+                return destroyReturn;
+            }
+            catch (error) {
+                fluid.log("Error destroying view component:", error);
+            }
+        }
+    };
+
     fluid.defaults("gpii.tests.handlebars.renderer.serverResourceAware.testEnvironment", {
         gradeNames: ["fluid.test.testEnvironment"],
+        events: {
+            constructFixtures: null,
+            onFixturesConstructed: null,
+            onMarkupRendered: null
+        },
+        invokers: {
+            destroyViewComponent: {
+                funcName: "gpii.tests.handlebars.renderer.serverResourceAware.testEnvironment.destroyViewComponent",
+                args: ["{that}"]
+            }
+        },
         components: {
             caseHolder: {
-                type: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder",
-                container: "body"
+                type: "gpii.tests.handlebars.renderer.serverResourceAware.testCaseHolder"
+            },
+            viewComponent: {
+                type: "gpii.tests.handlebars.templateAware.serverResourceAware",
+                createOnEvent: "{testEnvironment}.events.constructFixtures",
+                container: "body",
+                options: {
+                    model: {
+                        myvar:    "modelvariable",
+                        markdown: "*this works*",
+                        json:     { foo: "bar", baz: "quux", qux: "quux" }
+                    },
+                    templateName:            "index",
+                    selectors: {
+                        "viewport-after":       ".viewport-after",
+                        "viewport-append":      ".viewport-append",
+                        "viewport-before":      ".viewport-before",
+                        "viewport-html":        ".viewport-html",
+                        "viewport-prepend":     ".viewport-prepend",
+                        "viewport-replaceWith": ".viewport-replaceWith"
+                    },
+                    invokers: {
+                        "renderInitialMarkup": {
+                            funcName: "fluid.identity"
+                        }
+                    },
+                    listeners: {
+                        "onRendererAvailable.notifyParent": {
+                            func: "{testEnvironment}.events.onFixturesConstructed.fire"
+                        },
+                        "onMarkupRendered.notifyParent": {
+                            func: "{testEnvironment}.events.onMarkupRendered.fire"
+                        }
+                    }
+                }
             }
         }
     });
