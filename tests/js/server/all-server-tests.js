9108ea01503dd28ef0e8083133b31815139ca543
Tony Atkins <tony@raisingthefloor.org>
2019-10-11 09:22:58 +0200

    GPII-4100: Reordered node test rollup to avoid cross-talk between renderer and dispatcher tests.

--- tests/js/server/all-server-tests.js
+++ tests/js/server/all-server-tests.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 "use strict";
 
+// If this runs after the dispatcher tests the tests will fail because we are somehow getting the "secondary" templates.
+require("./standaloneRenderer-tests");
+
 // IoC tests must run first until this issue is resolved: https://issues.fluidproject.org/browse/FLUID-6397
 require("./dispatcher-tests.js");
 require("./errorRenderingMiddleware-tests");
@@ -9,8 +12,6 @@ require("./live-reload-tests");
 require("./resolver-tests");
 require("./singleTemplateMiddleware-tests");
 
-// If this runs after the dispatcher tests the tests will fail because we are somehow getting the "secondary" templates.
-require("./standaloneRenderer-tests");
 
 // Non-IoC tests
 require("./first-matching-path-tests");
